82b09fca7c8d9b19c3dd68ed4a4cf666e4cbae85
moataz <moatazemad772@gmail.com>
2023-03-03 03:15:17 +0200

    try to make it work on deployment by just edited the msg

--- src/controllers/usersControllers.ts
+++ src/controllers/usersControllers.ts
@@ -166,7 +166,7 @@ const UserController={
         const {email,uniqueString}=req.body;
         const existingUser=await User.findOne({email})
         if(!existingUser){
-            throw new BadReqErr('Can Not Find This Email.')
+            throw new BadReqErr('Can Not Find This Email Please Try Again.')
         }
 
         if(existingUser.uniqueResetPassStr!==uniqueString){
